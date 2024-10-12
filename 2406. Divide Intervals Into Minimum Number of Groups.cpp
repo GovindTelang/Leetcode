d4a20e8a646473a39114c9d6af766ddacdd9ee66
Govind Telang <133373913+GovindTelang@users.noreply.github.com>
2024-10-12 09:11:14 +0530

    Create 2406. Divide Intervals Into Minimum Number of Groups.cpp

---
+++ 2406. Divide Intervals Into Minimum Number of Groups.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    int minGroups(vector<vector<int>>& intervals) {
+       vector<int>arr,dep;
+        int n=intervals.size();
+        for(auto x:intervals){
+            arr.push_back(x[0]);
+            dep.push_back(x[1]);
+        }
+        sort(arr.begin(),arr.end());
+        sort(dep.begin(),dep.end());
+        int i = 1, count = 1;
+        int j = 0, ans = 1;
+        while (i < n && j < n)
+        {
+            if (arr[i] <= dep[j]) // one more platform needed
+            {
+                count++;
+                i++;
+            }
+            else // one platform can be reduced
+            {
+                count--;
+                j++;
+            }
+            ans = max(ans, count);
+        }
+        return ans;
+    }
+};
