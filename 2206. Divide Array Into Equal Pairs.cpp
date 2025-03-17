e896ea4ca18bce67022f494ddb7d6a572a93b092
Govind Telang <133373913+GovindTelang@users.noreply.github.com>
2025-03-17 21:15:44 +0530

    Create 2206. Divide Array Into Equal Pairs.cpp

---
+++ 2206. Divide Array Into Equal Pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> frequency;
+        for (int num : nums) {
+            frequency[num]++;
+        }
+
+        // Check if each number appears an even number of times
+        for (auto& entry : frequency) {
+            if (entry.second % 2 != 0) {
+                return false;
+            }
+        }
+
+        // All numbers can be paired
+        return true;
+    }
+};
