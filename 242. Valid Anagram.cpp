26b09458698e8f69bccee5aedcb661743c89aec1
Govind Telang <133373913+GovindTelang@users.noreply.github.com>
2025-07-25 17:56:45 +0530

    Create 242. Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        vector<int> freq(26,0);
+        for(char ch : s) freq[ch-'a']++;
+        for(char ch : t) freq[ch-'a']--;
+        for(int f :  freq) if(f!=0) return 0;
+        return 1;
+    }
+};
